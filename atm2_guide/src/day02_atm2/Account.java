c128e4e7d46968bcfb995e1bf9b2acb481fe9919
Administrator <Administrator@322-19>
2022-06-03 18:49:18 +0900

    atm2_guide

---
+++ atm2_guide/src/day02_atm2/Account.java
@@ -0,0 +1,6 @@
+package day02_atm2;
+
+public class Account {
+	String number;
+	int money;
+}
---
+++ atm2_guide/src/day02_atm2/_Main.java
@@ -0,0 +1,10 @@
+package day02_atm2;
+
+public class _Main {
+	public static void main(String[] args) {
+
+		ATM atm = new ATM();
+		atm.menu();
+		
+	}
+}
