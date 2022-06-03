040dcdea0cf6667091ed0a1c8d9734e63a76d7c7
Administrator <Administrator@322-19>
2022-06-03 18:44:46 +0900

    atm1_guide

---
+++ atm1/src/day01_atm/User.java
@@ -0,0 +1,9 @@
+package day01_atm;
+
+public class User {
+	String id = "";											// 아이디
+	String pw = "";											// 비밀번호
+	
+	Account[] acc = new Account[UserManager.ACC_MAX_CNT];	// 계좌정보
+	int accCnt = 0;											// 개인 보유 계좌 수
+}
---
+++ atm1/src/day01_atm/_Main.java
@@ -0,0 +1,13 @@
+package day01_atm;
+
+public class _Main {
+	public static void main(String[] args) {
+
+		Bank.instance.name = "GREEN";
+		BankManager.instance.play();
+		
+	}
+}
+
+
+
