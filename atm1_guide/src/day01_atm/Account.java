67d552f84aaa45f720de27dce4ea7dd2ed0ca5c0
Administrator <Administrator@322-19>
2022-06-03 18:46:16 +0900

    atm1_guide

---
+++ atm1_guide/src/day01_atm/Account.java
@@ -0,0 +1,8 @@
+package day01_atm;
+
+public class Account {
+	
+	int number = 0;			// 계좌번호
+	int money = 0;			// 계좌잔액
+	
+}
---
+++ atm1_guide/src/day01_atm/Bank.java
@@ -0,0 +1,10 @@
+package day01_atm;
+
+public class Bank {
+
+	static Bank instance = new Bank();
+	
+	String name = "";
+	int log = -1;
+	
+}
